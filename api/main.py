4e5b3590cc2787a65a5541f327bb402a750ac3ba
riyaraj23 <riyaarajj2002@gmail.com>
2026-04-04 11:45:53 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 
-from fastapi import FastAPI, UploadFile, File 
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -8,31 +9,25 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 MODEL = tf.keras.models.load_model("../models/10")
 
 CLASS_NAMES = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
-
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
 
-
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
-
 
 @app.post("/predict")
 async def predict(
@@ -40,9 +35,7 @@ async def predict(
 ):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-
     predictions = MODEL.predict(img_batch)
-
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
